6234ef3a48c9a77a8c6ff3987e9e7de71b2ef384
Fabi <fabianplum@web.de>
2025-10-13 14:46:08 +0200

    Added dynamic keypoint size scaling to better plot on images of different resolution

--- smal_fitter/draw_smal_joints.py
+++ smal_fitter/draw_smal_joints.py
@@ -8,7 +8,7 @@ import config
 
 class SMALJointDrawer():
     @staticmethod
-    def draw_joints(image, landmarks, visible = None):
+    def draw_joints(image, landmarks, visible = None, image_size = None):
         image_np = np.transpose(image.cpu().data.numpy(), (0, 2, 3, 1))
         landmarks_np = landmarks.cpu().data.numpy()
         if visible is not None:
@@ -16,16 +16,30 @@ class SMALJointDrawer():
         else:
             visible_np = visible
 
-        return_stack = SMALJointDrawer.draw_joints_np(image_np, landmarks_np, visible_np)
+        return_stack = SMALJointDrawer.draw_joints_np(image_np, landmarks_np, visible_np, image_size)
         return torch.FloatTensor(np.transpose(return_stack, (0, 3, 1, 2)))
 
     @staticmethod
-    def draw_joints_np(image_np, landmarks_np, visible_np = None):
+    def draw_joints_np(image_np, landmarks_np, visible_np = None, image_size = None):
         image_np = (image_np * 255.0).astype(np.uint8)
 
         bs, nj, _ = landmarks_np.shape
         if visible_np is None:
             visible_np = np.ones((bs, nj), dtype=bool)
+
+        # Calculate dynamic scaling based on image size
+        # Default size is optimized for 512x512 images
+        if image_size is None:
+            # Use the actual image dimensions if not provided
+            image_size = image_np.shape[1]  # Assuming square images, use height
+        
+        # Scale factor: 512 is the reference size
+        scale_factor = image_size / 512.0
+        
+        # Scale marker size and thickness
+        # Base values: marker_size=8, thickness=3 (optimized for 512x512)
+        marker_size = max(2, int(8 * scale_factor))
+        thickness = max(2, int(3 * scale_factor))
 
         return_images = []
         for image_sgl, landmarks_sgl, visible_sgl in zip(image_np, landmarks_np, visible_np):
@@ -38,7 +52,7 @@ class SMALJointDrawer():
                     x_co, y_co = inv_ctr * 10, 0
                     inv_ctr += 1
                 cv2.drawMarker(image_sgl, (int(x_co), int(y_co)), (int(color[0]), int(color[1]), int(color[2])),
-                               marker_type, 8, thickness = 3)
+                               marker_type, marker_size, thickness = thickness)
 
             return_images.append(image_sgl)
 
