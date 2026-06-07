c45ff88439681f9e03c37618199e7ccc82908360
Fabi <fabianplum@web.de>
2026-06-07 11:36:02 +0200

    Update version number

--- 3D_model_prep/SMIL_processing_addon.py
+++ 3D_model_prep/SMIL_processing_addon.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "SMIL Model Importer",
     "author": "Fabian Plum",
-    "version": (1, 2, 0),
+    "version": (1, 3, 0),
     "blender": (4, 2, 0),
     "location": "View3D > Tool Shelf",
     "description": "Import, configure, and export SMPL / SMIL models",
